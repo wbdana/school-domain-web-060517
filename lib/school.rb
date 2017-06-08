1fcfddc60110132793b61973b6f43a6ddb5dd0f2
wbdana <william.b.dana@gmail.com>
2017-06-08 14:54:00 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry-byebug'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+    @classes = {}
+  end
+
+  attr_accessor :roster
+
+  def add_student(student, grade)
+    if @roster[grade].class == Array
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.each do |grade, student_array|
+      @roster[grade] = student_array.sort!
+    end
+  end
+
+end
